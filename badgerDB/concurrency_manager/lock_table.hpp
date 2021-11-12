6ccbb4d2a5b58f9d2dddddac36d41ff3e8de6279
tmatsuzawa2 <tmatsuzawa@wisc.edu>
2021-11-12 01:44:57 -0600

    Added Global Lock

---
+++ badgerDB/concurrency_manager/lock_table.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../file_manager/block_id.hpp"
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+class lock_table {
+    private:
+        const int lockTime = 5000;
+        queue<block_id *> wait_list;
+        mutex m;
+        condition_variable cv;
+    
+    public:
+        lock_table();
+        void global_lock(block_id * p_blk);
+        void global_unlock();
+};
